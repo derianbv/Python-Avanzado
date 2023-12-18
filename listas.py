15562e9fd7fd50f16876822175a38562ef5fa9d1
derianbv <derojasl@unal.edu.co>
2023-12-18 00:51:59 -0500

    agregé fucniones map y lambda

--- listas.py
+++ listas.py
@@ -1,5 +1,5 @@
 ##################### Listas ##################################################
-
+################################################################################
 
 # 1. Listas: arreglo dinámico, se implementan por medio de arreglos estáticos que al llenarse se duplica su tamaño.    
 # 2. Es de la clase "Lista"
@@ -36,9 +36,19 @@ print(lista3)
 #en este caso los ementos de la lista tres van a hacer los que esten por dentro de la lista1 y lista2.
 
 
+
+
+
+
+
+
  ###################################### Métodos de las listas ##############################
 
 #1. Len(): largo lista
+
+
+
+
 #2. lista.index(a): devuelve el indice de la primera aparición de a en la lista
 
 #Se puede colocar un rango de busqueda, en el ejemplo de abajo va del indice 2 al indice 5
@@ -50,7 +60,13 @@ print(palabras.index("soy", 2, 5))
 #3. pop(): borra y retorna el último elemento de la lista.
 #pop(3): borra el elemento en la posición 3 
 
+
+
+
 #4. append(x): mete lo que sea que es x al final de la lista
+
+
+
 
 #5. extend(iterable): puede pegar dos listas: 
 
@@ -58,7 +74,36 @@ lista4 = ['a','b','c']
 lista4.extend(lista1)
 #['a', 'b', 'c', 1, 2, 3, 4]
 
+
+
+
 #6. insert(i, x): agrega en la posición i el elemento x: 
 lista4.insert(2,"ELEMENTO") 
 #Acá agrego en el indice 2 (originalemente estaba la c) el string ELEMENTO, esto corre a la "c" a la derecha.
 #print() = ['a', 'b', 'ELEMENTO', 'c', 1, 2, 3, 4]
+
+#Qué sucede si le paso un indice fuera de la lista? 
+
+lista4.insert(4444,"ÚLTIMO")
+#Lo pone al final de la lista y ya: 
+#print() = ['a', 'b', 'ELEMENTO', 'c', 1, 2, 3, 4, 'ÚLTIMO']
+
+
+
+
+#7. remove(x): remueve la primera instancia del elemento que encuentre de izq a derecha
+lista4.remove('a')
+#['b', 'ELEMENTO', 'c', 1, 2, 3, 4, 'ÚLTIMO']
+
+#remove(x): elimina la primera aparición de x,
+#pop(): elimina y retorna el último elemento de x, 
+#pop(i): elimina y retorna el elemento en i. 
+
+
+#8. clear(): elimina todos los elementos de la lista. 
+
+
+
+
+#9. sort(): 
+
