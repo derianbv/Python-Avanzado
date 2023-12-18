5f4e32c73e4323ac35208557bea1e9b20dd0681b
derianbv <derojasl@unal.edu.co>
2023-12-17 21:18:42 -0500

    Create listas.py

---
+++ listas.py
@@ -0,0 +1,13 @@
+##################### Listas ##################################################
+
+
+# 1. Listas: arreglo dinámico, se implementan por medio de arreglos estáticos que al llenarse se duplica su tamaño.    
+# 2. Es de la clase "Lista"
+# 3. Es mutable. 
+
+
+lista:list = []
+
+#4. Se pueden meter todo tipo de datos a la lista: 
+ 
+lista2 = []
