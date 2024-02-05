5fc02c2f7dfc206fc44670cdd7e7ee61e223a2d0
derianbv <derojasl@unal.edu.co>
2024-02-05 16:49:42 -0500

    retorno

--- listas.py
+++ listas.py
@@ -118,31 +118,31 @@ print(lista3)
 
  ###################################### Métodos de las listas ##############################
 
-#1. Len(): largo lista
+#1. len(lista): largo de la lista
 
 
 
 
-#2. lista.index(a): devuelve el indice de la primera aparición de a en la lista
+#2. lista.index(a,inicio,final): devuelve el indice de la primera aparición de a en la lista
 
-#Se puede colocar un rango de busqueda, en el ejemplo de abajo va del indice 2 al indice 5
+#Se puede colocar un rango de busqueda, en el ejemplo de abajo va del indice 2 al indice 5 (por defecto no se piden)
 #busque de izquierda a deracha, así que si se pone el inicio del rango de búsqueda en el ultimo item, va a salir que el elemento no está en la lista así sí esté 
 palabras = ["Yo", "soy", "un", "Yo", "soy", "Pythonista"]
 
 print(palabras.index("soy", 2, 5)) 
 
-#3. pop(): borra y retorna el último elemento de la lista.
-#pop(3): borra el elemento en la posición 3 
+#3. lista.pop(): borra y retorna el último elemento de la lista.
+# lista.pop(3): borra el elemento en la posición 3 
 
 
 
 
-#4. append(x): mete lo que sea que es x al final de la lista
+#4. lista.append(x): mete lo que sea que es x al final de la lista
 
 
 
 
-#5. extend(iterable): puede pegar dos listas: 
+#5. lista.extend(iterable): puede pegar dos listas: 
 
 lista4 = ['a','b','c']
 lista4.extend(lista1)
@@ -151,7 +151,7 @@ lista4.extend(lista1)
 
 
 
-#6. insert(i, x): agrega en la posición i el elemento x: 
+#6. lista.insert(i, x): agrega en la posición i el elemento x: 
 lista4.insert(2,"ELEMENTO") 
 #Acá agrego en el indice 2 (originalemente estaba la c) el string ELEMENTO, esto corre a la "c" a la derecha.
 #print() = ['a', 'b', 'ELEMENTO', 'c', 1, 2, 3, 4]
@@ -165,10 +165,11 @@ lista4.insert(4444,"ÚLTIMO")
 
 
 
-#7. remove(x): remueve la primera instancia del elemento que encuentre de izq a derecha
+#7. lista.remove(x): remueve la primera instancia del elemento que encuentre de izq a derecha
 lista4.remove('a')
 #['b', 'ELEMENTO', 'c', 1, 2, 3, 4, 'ÚLTIMO']
 
+#---------- comparación ---------------
 #remove(x): elimina la primera aparición de x,
 #pop(): elimina y retorna el último elemento de x, 
 #pop(i): elimina y retorna el elemento en i. 
