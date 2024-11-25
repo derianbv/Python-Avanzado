59f7e3682201bbec71f85be4a3033c695c276224
derianbv <116598387+derianbv@users.noreply.github.com>
2024-11-24 20:49:23 -0500

    Update Strings.py

--- Strings.py
+++ Strings.py
@@ -22,7 +22,7 @@ print(str1[-9])
 #Usar "\" en el texto: \\ o agregar una r al lado del texto para cancelar estas scaped sequnces. Ej: r"Hola \ mundo" como raw
 
 #Métodos ----------------------------------------------------------------------------------------------------------
-#Strings have sequenced methods y metodos para texto"
+#Strings have sequenced methods y metodos para texto": ejemplo: .find(),.replace(),.split(): divide todo en un arreglo. 
 
 #1.Format: agarra un string interpolation para poner .format(va1...varn) en sus place holder.
 
@@ -31,9 +31,26 @@ age = 50
 print("My name is {} and I am {} years old.".format(name, age))
 #"My name is John and I am 50 years old."
 
+#2. str.upper(): todo a mayus
+
 ### String interpolation (f-string)--------------------------------------------------------------------------
 
 name = "John"
 age = 30
 print(f"My name is {name} and I am {age} years old.")
 
+########################### Regular expresion------------------------------------------------------------------------
+#Modulos de python "import r" que mejora las opciones de búsqueda en textos: 
+###:  ejemplo: \d detecta un numero del 0-9
+
+import re
+
+pattern = r"\d\d\d\d\d\d\d\d\d\d"  # Matches any ten consecutive digits
+text = "My Phone number is 1234567890" #el texto donde voy a buscar
+match = re.search(pattern, text) # el patron que voy a buscar
+
+#if match devuelve objeto match. sino, devuelve none
+if match:
+    print("Phone number found:", match.group())
+else:
+    print("No match")
