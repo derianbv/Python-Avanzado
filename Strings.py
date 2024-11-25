c7333d207b56060d44e6df0be369e273f3179911
derianbv <116598387+derianbv@users.noreply.github.com>
2024-11-24 20:37:12 -0500

    Update Strings.py

--- Strings.py
+++ Strings.py
@@ -19,10 +19,17 @@ print(str1[-9])
 #Carácteres especiales que modifican la manera en la que se muestran los strings
 #Salto de línea: \n 
 #Tab: \t 
-#Usar "\" en el texto: \\ o agregar una r al lado del texto para cancelar estas scaped sequnces. Ej: r"Hola \ mundo"
+#Usar "\" en el texto: \\ o agregar una r al lado del texto para cancelar estas scaped sequnces. Ej: r"Hola \ mundo" como raw
 
 #Métodos ----------------------------------------------------------------------------------------------------------
 #Strings have sequenced methods y metodos para texto"
+
+#1.Format: agarra un string interpolation para poner .format(va1...varn) en sus place holder.
+
+name = "John"
+age = 50
+print("My name is {} and I am {} years old.".format(name, age))
+#"My name is John and I am 50 years old."
 
 ### String interpolation (f-string)--------------------------------------------------------------------------
 
