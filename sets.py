07355811972fa0520979db08283d9f6d27389298
derianbv <116598387+derianbv@users.noreply.github.com>
2024-11-25 14:54:48 -0500

    Update sets.py

--- sets.py
+++ sets.py
@@ -24,13 +24,21 @@ Verificar si elemento está en set
 ############################## operaciones entre conjuntos ################################
 
 1. Y: items en ambos conjuntos: 
-conjunto1 & conjunto2 = 1,2. (1 y 2 estan en ambos) 
+A & B = 1,2. (1 y 2 estan en ambos) 
 
 2. Union U: pega todos los items 
-conjunto1.union(conjunto2)
+A.union(B)
 
-3. Checkear si Conjunto1 pertenece (o es subconjunto) del conjunto2: 
-conjunto1.issubset(conjunto2)
+3. Checkear si Conjunto1 A pertenece (o es subconjunto) del conjunto2 B: 
+A.issubset(B)
+- Para checkear si es conjunto padre: 
+A.issuperset(B) 
+
+4. Diferencia A - B: 
+A.difference(B) 
+
+5. A Interccion  B: 
+A.intersection(B) 
 
 '''
 
