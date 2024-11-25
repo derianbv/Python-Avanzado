b72afd243e88a289f99fbb5a1285f5e56b46a065
derianbv <116598387+derianbv@users.noreply.github.com>
2024-11-25 14:37:09 -0500

    Create sets.py

---
+++ sets.py
@@ -0,0 +1,21 @@
+######################################## SETSSSSS - CONJUNTOS ############################################################
+
+###### Características ########
+
+#Diagramas de benn - conjuntos 
+'''
+1. No están ordenados 
+2. Se crean como diccionarios pero solo con llaves 
+3. Si se repiten elementos en el set se van a dejar solo referenciado una vez.
+4. Se le pueden meter todos los tipos de datos al set. 
+
+
+############################## Funciones ################################
+1. conjunto.add("item")
+#el item entra al circulito (visto como un diagrama de ben) 
+
+2. conjunto.remove("item")
+
+
+'''
+
