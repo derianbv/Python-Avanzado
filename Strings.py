0fe71e1cf2e2b3ada6a169ed88c94ec2d549e351
derianbv <116598387+derianbv@users.noreply.github.com>
2024-11-24 20:31:23 -0500

    Update Strings.py

--- Strings.py
+++ Strings.py
@@ -11,4 +11,22 @@ len(str1)
 print(str1[-9])
 #H
 
+# Ver más de esto en slicing.py 
+
+
+######:  Scape sequences (\): -------------------------------------------------------------------------------------
+
+#Carácteres especiales que modifican la manera en la que se muestran los strings
+#Salto de línea: \n 
+#Tab: \t 
+#Usar "\" en el texto: \\ o agregar una r al lado del texto para cancelar estas scaped sequnces. Ej: r"Hola \ mundo"
+
+#Métodos ----------------------------------------------------------------------------------------------------------
+#Strings have sequenced methods y metodos para texto"
+
+### String interpolation (f-string)--------------------------------------------------------------------------
+
+name = "John"
+age = 30
+print(f"My name is {name} and I am {age} years old.")
 
