599b45084627ab34449f877fe2f7ca563200dc26
derianbv <116598387+derianbv@users.noreply.github.com>
2024-11-30 16:06:50 -0500

    Update ManejarArchivos.py

--- ManejarArchivos.py
+++ ManejarArchivos.py
@@ -1,6 +1,6 @@
   '''
 ################################ Leer archivos #############################################
-Podemos abrir archivos con la función archivo = open(path relativo, mode), mode puede ser puede ser los tipos de linux, read, write and execute, esto va a abrir un objeti tipo File
+Podemos abrir archivos con la función archivo = open(path relativo, mode), mode puede ser puede ser los tipos de linux, read, write,  binario, esto va a abrir un objeti tipo File
 
 Ejemplo: 
   archivo = open(path relativo, "r")
@@ -14,5 +14,13 @@ Ejemplo:
   with open('archivo.txt', 'w') as archivo:
     archivo.write('Hola, mundo!')  # El archivo se cierra automáticamente.
 
+
+
+    #################### Métodos #################################3
+
+    1. string1 = archivo.read(n): sin la n guarda todo los qué esté en el archivo de txt en una string, con n avanzará un puntero hasta n y guardará todo lo que recorra 
+    2. archivo.closed = true or false, muestra si está cerrado 
+    3. archivo.readline() = retorna cada línea en un espcacio de un arreglo. 
+    
     
   '''
