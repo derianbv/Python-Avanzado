0d44980c09684355f1ba9130ab5439d21d3c9865
derianbv <116598387+derianbv@users.noreply.github.com>
2024-11-30 16:58:04 -0500

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -11,7 +11,15 @@ los values ordenados en listas van a ser los rows, se asignan en orden entonces 
 ######################### Métodos ###########################
 '''
 1. var = pandas.read_csv(archivo ) Leer CSV, retorna en var una tabla con los archivos del csv, similar para excel .real_xlsx()
+
+
 2. buscar en las tablas: tabla.iloc[n,m]: (0 <= n,m)  n dice que busque en la fila n, y m en la columna m, ejemplo tabla.iloc(2,3) fila 3 columna 4 (porque empieza en 0), también pueden ir
 con el título de la columna, Ejemplo: tabla.iloc(2,"año_de_publicacion"): en la colum año de publicación vaya a la 3ra fila
+  en estos números se pueden usar rangos, ejemplo: nuevaTabla = tabla.iloc[0:2,0:3] : quiero una tabla que vaya por las dos primeras filas y las tres primeras columnas, en las columnas
+  también se pueden usar el nombre de las llaves tabla.iloc[0:2, 'título' : 'año ]
 
+
+
+
+3. tabla.index = [a,b,c]: cambia el index de la tabla de 0,1,3 a a,b,c PERO ahora para entrar a sus datos se pasa de iloc[] -> loc[]
 '''
