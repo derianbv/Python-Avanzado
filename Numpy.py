a5781d30c2cff6ac23937a9cec6177943d45270d
derianbv <116598387+derianbv@users.noreply.github.com>
2024-11-30 21:03:50 -0500

    Create Numpy.py

---
+++ Numpy.py
@@ -0,0 +1,26 @@
+'''
+:)  
+
+Es posible crear vectores y sumarlos. 
+vector = numpy.array([1,2,3,4])
+Los vectores son esencialmente listas sin embargo a la hora de las mates se comporta como un vector y sus propiedades: 
+
+Producto punto, producto cruz, multiplicacion de vectores 
+
+También Universal functions as could be: 
+vector.max(), vector.min(), mean() 
+
+
+Numpy tiene variables y funcione matematicas: 
+
+numpy.py, numpy.sin(x)
+
+
+
+
+###### 2D matrix ##################
+
+
+
+
+'''
