fd80ea329739f2050046b59314a1be250b6f7f3f
derianbv <116598387+derianbv@users.noreply.github.com>
2024-12-01 20:37:27 -0500

    Update API.py

--- API.py
+++ API.py
@@ -1,1 +1,37 @@
+# Rest API is a internet API 
+################################### EPP #################################################
+# Son dos piezas de software que hablan entre sí - como un traductor
+'''
+# Hay dos actores: yo (cliente) y un servidor (endpoint), nos mensajeamos por medio de HTTP messages con json insertado
+Los mensajes HTTP contienen: 
 
+--------Response Start Line ------------------------
+El CÓDIGO  de la request, lo qué le pido y para el server: 
+el resultado de la request OK or FAIL 
+-----------Responde Header------------------------
+Meta info 
+---------Response body----------------------------
+El archivo o mercancía que estaba pidiendo
+
+
+
+################## Códigos #######################
+-Prefijos indican la clase (1,2,3,4,5)
+1XX: Todo está yendo bien. 
+2XX: Exito 
+3XX 
+
+
+
+########################### REST - API ####################################
+
+Partes de la dirección web: 
+
+Scheme:  El protocolo por el que se van a enviar los archivos: http://
++
+Internet adress or Base URL: www.gitlab.com
++
+Route: /home
+
+
+'''
