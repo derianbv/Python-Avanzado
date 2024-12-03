071e07ff2ebcfa7a05c27499161b7fe50250a8c6
derianbv <116598387+derianbv@users.noreply.github.com>
2024-12-02 20:01:56 -0500

    Update API.py

--- API.py
+++ API.py
@@ -5,12 +5,12 @@
 # Hay dos actores: yo (cliente) y un servidor (endpoint), nos mensajeamos por medio de HTTP messages con json insertado
 Los mensajes HTTP contienen: 
 
---------Response Start Line ------------------------
+--------Response Start Line ------------------------ 
 El CÓDIGO  de la request, lo qué le pido y para el server: 
 el resultado de la request OK or FAIL 
------------Responde Header------------------------
+-----------Responde Header------------------------ HEAD
 Meta info 
----------Response body----------------------------
+---------Response body---------------------------- BODY
 El archivo o mercancía que estaba pidiendo
 
 
@@ -19,8 +19,9 @@ El archivo o mercancía que estaba pidiendo
 -Prefijos indican la clase (1,2,3,4,5)
 1XX: Todo está yendo bien. 
 2XX: Exito 
-3XX 
-
+3XX: refered
+4XX: Failed
+5XX: Server error. 
 
 
 ########################### REST - API ####################################
@@ -33,5 +34,31 @@ Internet adress or Base URL: www.gitlab.com
 +
 Route: /home
 
+########## HTTP #### Métodos ###########:
+1. Get: obtiene info de un server
+2. Post: sube info al server 
+3. Put: Actualiza ya info en el server 
+4. Delete: borra info del server 
+
+
+
+Libreria: import request ############# request ############# Library 
+
+la manera de obtener un server es 
+server = request.get(youtube.com)
+server.request.body -> relacionado con el body de un mensaje HTTP
+
+
+
+################## Queries #############################
+
+(http://youtube.com/)+(get)+(?)+(Name=Jose & ID=123
+?: Es el inicio de la query
+&: Separa parámetros 
+
+Ejemplo: 
+
 
 '''
+
+
