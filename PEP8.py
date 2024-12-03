096d74d9454eba38a3cbb85b7092ad5ee1d855d6
derianbv <116598387+derianbv@users.noreply.github.com>
2024-12-03 18:13:51 -0500

    Update and rename BestPractices.py to PEP8.py

---
+++ PEP8.py
@@ -0,0 +1,18 @@
+'''
+Teniendo en cuenta los lineamientos de PEP8 guidelines
+Indentación: 
+1. Utilizar siempre solo un espacio para indentar y no tab ya que el tañamo de tab varia con los text editors, cuando se use tab entonces 4 espacios 
+2. Separar las funciones o clases con un enter.  
+3. Utilizar espaciones 
+Ejemplos a=b+c X, a = b + c BIENNN
+4. Para todos los bloques de código siempre escribirlos en funciones, esto hace el código más limpio y mejora su performance
+
+5. Nombrar a las funciones con under scores y minúscula y NO USAR CAMEL case: 
+ mi_funcion() 
+ Esta regla no aplica para paquetes, ahí es mejor solo el nombre y ya: mipaquete
+  6. Para las clases usar Camel case: 
+    AnimalMarino Class
+    7. Constantes con mayus y separadas con underscore: VALOR_PI
+
+
+'''
