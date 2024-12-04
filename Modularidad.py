52c6db337be1fa64494bf8f5ab1b77ad16b982c9
derianbv <116598387+derianbv@users.noreply.github.com>
2024-12-03 23:18:01 -0500

    Create Modularidad.py

---
+++ Modularidad.py
@@ -0,0 +1,11 @@
+'''
+1. Debemos crear varios archivos de python separados en módulos (modularidad) a la hora de crear nuestro app debe 
+llamarse __inti__.py y en este archivo se deben llamar a las cosas que necesitemos de los otros modulos con un from . import module1 
+
+
+
+
+
+
+
+'''
