b52a226087321d8efbd942c1cf7da99e5b41112e
derianbv <116598387+derianbv@users.noreply.github.com>
2024-12-06 21:12:50 -0500

    Update Flask.py

--- Flask.py
+++ Flask.py
@@ -1,1 +1,8 @@
+#Es una libreria liviana para realizar páginas web. 
 
+#Importación: 
+from flask import Flask #Flas está en mayúscula porque es una clase
+app = Flask(__name__) #Acá se utiliza el cosntructor de flask y se le pasa name 
+@app.route('/')
+def hello_world():
+    return "<b> Hola mama </b>"
