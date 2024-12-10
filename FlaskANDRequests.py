3c936974d3eea1754972c9074345673a516cb77a
derianbv <116598387+derianbv@users.noreply.github.com>
2024-12-10 15:16:51 -0500

    Update FlaskANDRequests.py

--- FlaskANDRequests.py
+++ FlaskANDRequests.py
@@ -32,11 +32,11 @@ antes, esta función solo sería una función más en el archivo.
 '''
 
 def hello_world(): #Acá el método Get implicito, 
-    return "<b> Hola mama </b>"
+    return ("<b> Hola mama </b>", 200) #Acá le digo que le pase el codigo 200 si todo bn
 
 
 @app.route('/index', methods=["GET", "POST"])
-def index():
+def index(): #Esta función responde a las solicitudes de los clientes cuando entren aquí
     if request.method == "GET" return jsonify(status="OK", method="GET"), 200
     if request.method == "POST" return jsonify(status="OK", method="POST"), 200
 
@@ -44,11 +44,39 @@ def index():
 
 @app.route("/")
 def get_author():
+    #res es un objeto tipo response que tendrá muchas funciones tales como response.status_code() para tener el codigo de respuesta o 
+    #res.json() para parsear el objeto a un diccionario si la respuesta es un json.
     res = requests.get("https://openlibrary.org/search/authors.JSON?q=Michael Crichton") # Le estoy pidiendo al servidor que se contecte al
-    #servidor de la URL y le haga una petición GET 
-    if res.status_code == 200:
+    #servidor de la URL y le haga una petición GET y retorna un objeto tipo Response 
+    if res.status_code == 200: #si la respuesta es exitosa
         return {"message": res.json()}
+    elif res.status_code == 404: #Si la respuesta no es exitosa
+        return {"message": "Something went wrong!"}, 404
+    else:
+        return {"message": "Server error!"}, 500
+
+####################################### Rutas dinámicas ###################################################
+
+@app.route("/book/<string:isbn>") #con string:isbn lo paso a sttring
+#<isbn> es un parámetro dinámico de la URL. Esto significa que si un usuario visita /book/12345, 
+#el valor "12345" se asignará a la variable isbn dentro de la función.#
+def get_author(isbn): #hay que pasarle la var dinámica
+    res = requests.get(f"https://openlibrary.org/isbn/{escape(isbn)}.JSON") #Acá hace un llamado a la Api de open library con un GET
+
+    '''
+    escape(isbn) se utiliza para asegurarse de que la variable isbn no contenga caracteres que pudieran causar problemas de seguridad o interpretación. 
+    Esto es especialmente importante si isbn proviene de una entrada de usuario o de una fuente externa no confiable.
+    '''
+    if res.status_code == 200:
+        return {"message": res.json()} #este json probablemente tiene el libro
     elif res.status_code == 404:
         return {"message": "Something went wrong!"}, 404
     else:
         return {"message": "Server error!"}, 500
+
+
+
+
+##################### ERRORES #######################################
+
+
