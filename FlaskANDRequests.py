88ed4274e1a91b936155e7e091ec519ac68ce81a
derianbv <116598387+derianbv@users.noreply.github.com>
2024-12-10 18:06:17 -0500

    Update FlaskANDRequests.py

--- FlaskANDRequests.py
+++ FlaskANDRequests.py
@@ -152,3 +152,23 @@ Connection: close
 
 
 '''
+
+######################## RENDERIZARRRRRRRRRRRRRRRRRRR ##########################################
+from flask import Flask, render_template, request
+app = Flask("My first Application")
+
+@app.route('/sample')
+def getSampleHtml():
+    return render_template('sample.html') #en esta vista rederizo el html sample.html
+
+@app.route('/user/<username>', methods=['GET'])
+def greetUser(username): ########## a esta vista le estoy pasando al html la variable que agarro de la url (usarname)
+    return render_template("result.html", username=username)
+
+@app.route('/user', methods=['GET'])
+def greetUserBasedOnReq():
+    username = request.args.get("username")
+    return render_template("result.html", username=username)
+
+if __name__ == "__main__":
+    app.run(debug=True)
