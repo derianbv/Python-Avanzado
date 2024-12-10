4eadc54dffd670308a5da580c0506c0c7229514d
derianbv <116598387+derianbv@users.noreply.github.com>
2024-12-10 14:59:37 -0500

    Update modulos.py

--- modulos.py
+++ modulos.py
@@ -1,4 +1,16 @@
 #################### Module Operator #############################
 
-#import re: 
+#import requests  
 #operate strings 
+
+#Cuando realizo un import libreria: 
+import request 
+#Python busca en este orden en sus carpetas internas: 
+# 1. Las libreria estandar de python. 
+# 2. La carpeta en dónde tengo el archivo 
+# 3. La libreria site-packages de python en dónde se descargan archivos pip
+  '''
+  Después cargará en un archivo en bytecode .pyc que crea todas sus funciones y variables en memoria para usarlas facilmente por el 
+  archivo python  
+
+  '''
