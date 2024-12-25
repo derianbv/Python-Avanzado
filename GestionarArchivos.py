05e20a6aa8030d063781d8a92843fbfabafe38ae
derianbv <116598387+derianbv@users.noreply.github.com>
2024-12-25 15:40:55 -0500

    Update GestionarArchivos.py

--- GestionarArchivos.py
+++ GestionarArchivos.py
@@ -3,9 +3,19 @@
 1. Ruta relativa (en relación con la carpeta actual) 
 Se basa en un sitema que se establece con la carpeta actual, es decir, para acceder a algo que este en la misma carpeta, por ejemplo: 
 
-Proyecto/
-         main.py 
-         assets/
-                imagen1.png
-                convertor.py 
-'''
+proyecto/
+    main.py
+    utils/
+        __init__.py
+        calculadora.py
+        conversor.py
+
+Inicialmente, para volver una carpeta un paquete y poder llamarlo en otros, se debe usar __init__.py en esa carpeta:
+
+# EN EL ARCHIVO: __init__.py:
+#Suponiendo que hay una carpeta Utils que tiene a los archivos calculadora y conversor.py, que estas tienen funciones sumar e celcius a farenheit respectivamente: 
+
+from .calculadora import sumar
+from .conversor import celsius_a_fahrenheit
+
+# Ahora puedo llamarlas en otro el archivo main.py: 
