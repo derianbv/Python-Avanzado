9e5edb829836797622c350c1fe371115b93269c5
derianbv <116598387+derianbv@users.noreply.github.com>
2024-12-25 15:21:34 -0500

    Create GestionarArchivos.py

---
+++ GestionarArchivos.py
@@ -0,0 +1,11 @@
+##########################################  RUTAS ############################################################
+'''
+1. Ruta relativa (en relación con la carpeta actual) 
+Se basa en un sitema que se establece con la carpeta actual, es decir, para acceder a algo que este en la misma carpeta, por ejemplo: 
+
+Proyecto/
+         main.py 
+         assets/
+                imagen1.png
+                convertor.py 
+'''
