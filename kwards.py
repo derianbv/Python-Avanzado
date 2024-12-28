4dc913f93d683cd761d9332650db093ec79889e4
Derian Rojas <116598387+derianbv@users.noreply.github.com>
2024-12-27 20:59:55 -0500

    Update kwards.py

--- kwards.py
+++ kwards.py
@@ -1,5 +1,18 @@
 ################################# Kwards ############################################### :p 
+'''
+--------------------------------Tipos de parámetros------------:
 
 
 
+Tipo de Parámetro	      | Ejemplo de Definición	|  Ejemplo de Llamada
+------------------------|-----------------------|-----------------------
+-Posicional	            |def func(a, b):	      |   func(1, 2)
+-Con nombre	            |def func(a, b):	      |func(a=1, b=2)
+-Con valor por defecto	|def func(a, b=10):	    |func(1) o func(1, 20)
+-Posicional solamente	  |def func(a, b, /):	    |func(1, 2)
+-Con nombre solamente	  |def func(*, a, b):	    |func(a=1, b=2)
+-Posicionales variables	|def func(*args):	      |func(1, 2, 3)
+-Con nombre variables	  |def func(**kwargs):	  |func(a=1, b=2)
 
+
+1. posicional: importa el orden, en def func(a, b) si le paso func(1, 2) entonces a=1, b=2 
