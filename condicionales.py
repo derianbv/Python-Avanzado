2f7daec89c936699967e63a3614da5b951a2016e
Derian Rojas <116598387+derianbv@users.noreply.github.com>
2025-01-01 23:46:46 -0500

    Update condicionales.py

--- condicionales.py
+++ condicionales.py
@@ -1,1 +1,14 @@
 ################################## Condicionales ################################################
+
+###################### is ###########################
+#1. Verifica si dos variables apuntan al mismo espacion en memoria: 
+
+x = 10
+y = 10
+
+print(x is y)  # True: Ambos usan la misma referencia en memoria, los numeros pequeños apuntan a un mismo lugar en memoria 
+
+z = 1000
+w = 1000
+
+print(z is w)  # True o False, depende de la implementación
