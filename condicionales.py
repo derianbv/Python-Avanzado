02f4a691140aca717694ab26b8a1a23ac7907305
Derian Rojas <116598387+derianbv@users.noreply.github.com>
2025-01-01 23:50:41 -0500

    Update condicionales.py

--- condicionales.py
+++ condicionales.py
@@ -1,7 +1,12 @@
 ################################## Condicionales ################################################
 
 ###################### is ###########################
-#1. Verifica si dos variables apuntan al mismo espacion en memoria: 
+'''
+- is	La identidad del objeto	| a is b → True si a y b son el mismo objeto.
+- ==	Los valores (igualdad)	| a == b → True si los valores son iguales.
+'''
+
+#1. Verifica si dos variables apuntan al mismo espacion en memoria, esencialmente si dos objetos son el mismo: 
 
 x = 10
 y = 10
@@ -12,3 +17,25 @@ z = 1000
 w = 1000
 
 print(z is w)  # True o False, depende de la implementación
+
+#También para las clases-------------------------------------------------------------------: 
+
+class MyClass:
+    pass
+
+obj1 = MyClass()
+obj2 = obj1 #
+obj3 = MyClass()
+
+print(obj1 is obj2)  # True: Mismo objeto
+print(obj1 is obj3)  # False: Diferentes instancias
+
+########## Diferencia con ==: 
+
+a = [1, 2, 3]
+b = [1, 2, 3]  # b es un objeto diferente con el mismo contenido
+
+print(a is b)  # False: No son el mismo objeto
+print(a == b)  # True: Los valores son iguales
+
+s
